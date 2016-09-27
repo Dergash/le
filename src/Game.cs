cfc5da8d79ba607ad0ade820a0359e8ca603b600
Dergachev <dergachev@hotmail.com>
2016-09-27 22:37:19 +0300

    feat(render): add basic support for image loading (so far only square images supported)

--- src/Game.cs
+++ src/Game.cs
@@ -4,10 +4,11 @@ using System.IO;
 
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
-using Drawing = ImageProcessorCore;
 
 namespace LE {
     public class Game : GameWindow {
+
+        int? backgroundTextureId;
 
         public Game() {
             base.Title = "Princess colour";
@@ -15,67 +16,43 @@ namespace LE {
         protected override void OnResize(EventArgs e)
         {
             GL.Viewport(0, 0, this.Width, this.Height);
+            renderEmptyBackground();
+            if(this.backgroundTextureId.HasValue) {
+                renderBackground();
+            }
+            this.SwapBuffers();
         }
         protected override void OnLoad(EventArgs e) {
-           // base.OnLoad(e);
-
-
-           // GL.ClearColor(10.0f, 110.0f, 0.0f, 0.0f);
-            
-           // int textureId = loadTexture("samples/area.bmp");
-           // GL.BindTexture(TextureTarget.Texture2D, textureId);
-           Color color = new Color(32,50,108,0);
-         //  GL.ClearColor()
-            GL.ClearColor(color);
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-        
-            this.SwapBuffers();
-
+            GL.Enable(EnableCap.Texture2D);
+            var texture = new Texture("samples/forest.png");
+            if(texture.Id != -1) {    
+                this.backgroundTextureId = texture.Id;
+            }
         }
         protected override void OnRenderFrame(FrameEventArgs e) {
-            //Matrix4 modelView = Matrix4.LookAt(Vector)
-         /*   Bitmap areaBackground = new Bitmap();
-            int textureId = loadTexture("samples/area.bmp");
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadIdentity();
-            GL.Ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadIdentity();
-
-            GL.Enable(EnableCap.Texture2D);
-            GL.BindTexture(TextureTarget.Texture2D, textureId);
-
-            GL.Begin(PrimitiveType.Quads);
-
-            GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(-1.0f, -1.0f);
-            GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(1.0f, -1.0f);
-            GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(1.0f, 1.0f);
-            GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(-1.0f, 1.0f);
-
-            GL.End();*/
+            renderEmptyBackground();
+            if(this.backgroundTextureId.HasValue) {
+                renderBackground();
+            }
+            this.SwapBuffers();
         }
 
-        protected int loadTexture(String filename) {
-            if (String.IsNullOrEmpty(filename)) {
-                throw new ArgumentException(filename);
-            }
-            int id = GL.GenTexture();
-            using(FileStream stream = File.OpenRead(filename)) {
-                Drawing.Image image = new ImageProcessorCore.Image(stream);
-               // byte[ , , ] data = new byte[image.Height, image.Width, 3];
-                byte[] pixels = new byte[image.Height * image.Width * 3];
+        void renderEmptyBackground() {
+            Color color = new Color(32, 50, 108, 0);
+            GL.ClearColor(color);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        }
 
-                for(int i = 0; i < image.Pixels.Length * 3; i++) {
-                    pixels[i] = 33;
-                }
-                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height,
-                    0, PixelFormat.Bgra, PixelType.UnsignedByte, pixels);
-                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-                
-                //GL.BindTexture(TextureTarget.Texture2D, 0);
-            }
-            return id;
+        void renderBackground() {
+            GL.BindTexture(TextureTarget.Texture2D, this.backgroundTextureId.Value);
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.TexCoord2(0, 0);  GL.Vertex2(-1, 1);
+            GL.TexCoord2(1, 0);  GL.Vertex2(1, 1);
+            GL.TexCoord2(1, 1);  GL.Vertex2(1, -1);
+            GL.TexCoord2(0, 1);  GL.Vertex2(-1, -1);
+
+            GL.End();
         }
     }
 }
---
+++ src/Render/Texture.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using Drawing = ImageProcessorCore;
+
+namespace LE {
+    public class Texture {
+        public readonly int Id;
+        public readonly uint Width;
+        public readonly uint Height;
+        public Texture(String Filename) {
+            if (String.IsNullOrEmpty(Filename)) {
+                throw new ArgumentException(Filename);
+            }
+
+            Drawing.Image image;
+            if(!File.Exists(Filename)) {
+                this.Id = -1;
+                return;
+            };
+            
+            using(FileStream stream = File.OpenRead(Filename)) {
+                image = new ImageProcessorCore.Image(stream);
+            }
+
+            this.Width = (uint)image.Width;
+            this.Height = (uint)image.Height;
+
+            byte[] pixels = getBGRFromImage(image);
+
+            this.Id = getSquareTexture(pixels, Width, Height);
+        }        
+
+        int getSquareTexture(byte[] Pixels, uint Width, uint Height) {
+            int id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, id);
+    
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, (int)Height, (int)Width,
+                0, PixelFormat.Bgr, PixelType.UnsignedByte, Pixels);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
+            
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+            return id;
+        }
+
+        byte[] getBGRFromImage(Drawing.Image Image) {
+            byte[] pixels = new byte[Image.Height * Image.Width * 3];
+            for(uint i = 0, j = 0; i < Image.Pixels.Length; i++, j += 3) {
+                uint blue = j;
+                uint green = j + 1;
+                uint red = j + 2;
+                pixels[blue] = Image.Pixels[i].B;
+                pixels[green] = Image.Pixels[i].G;
+                pixels[red] = Image.Pixels[i].R;
+            }
+            return pixels;
+        }
+    }
+}
